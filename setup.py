ef5da4e10f83d6fba15898b2bdbc6de77e07546f
caixuefei <44161164+caixuefei@users.noreply.github.com>
2020-05-22 15:16:21 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='bumblebee_env',
+setup(name='gym_bumblebee',
       version='0.0.1',
       install_requires=['gym']  # And any other dependencies foo needs
 )
