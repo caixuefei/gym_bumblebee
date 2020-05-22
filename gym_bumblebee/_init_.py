9d08d73e636a2ae348992d11548230d1e9b92a8a
caixuefei <44161164+caixuefei@users.noreply.github.com>
2020-05-22 16:35:05 +0900

    Update _init_.py

--- gym_bumblebee/_init_.py
+++ gym_bumblebee/_init_.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='bumblebee-v0',
-    entry_point='gym_bumblebee.envs:BumblebeeEnv',
+    entry_point='gym_bumblebee.env:BumblebeeEnv',
 )
